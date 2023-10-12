134698e965307607dd0b5ee4cd042cad9df12deb
Samsonium <tdesu@vk.com>
2023-10-12 11:57:40 +0300

    Add index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import fetchApi from './fetchApi';
+
+import APIRecord from './interfaces/APIRecord';
+import APIRequestInit from './interfaces/APIRequestInit';
+
+export {
+    fetchApi as default,
+    APIRequestInit,
+    APIRecord
+};
