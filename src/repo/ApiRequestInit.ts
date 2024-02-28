04a302be393b632ee744faafaa215346c56d5252
samsonium <tdesu@vk.com>
2024-02-28 23:15:41 +0300

    Add fetch API field to `extra` field in request init

--- src/repo/ApiRequestInit.ts
+++ src/repo/ApiRequestInit.ts
@@ -7,7 +7,73 @@
  */
 export type ApiRequestInit<Q, P, B> = Partial<{
     /** Query parameters */
-    query?: Q;
+    query: Q;
+
+    /** Should throw an error if request fails */
+    throwsOnError: boolean;
+
+    /** List of headers that will be applied to request */
+    headers: Record<string, string | number | boolean>;
+
+    /** Extra options for Fetch API */
+    extra: Partial<{
+
+        /**
+         * Interaction with browser's HTTP cache
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#credentials
+         */
+        cache: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached';
+
+        /**
+         * What browser need to do with credentials
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#credentials
+         */
+        credentials: 'omit' | 'same-origin' | 'include';
+
+        /**
+         * Allow the request to outlive the page
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#keepalive
+         */
+        keepalive: boolean;
+
+        /**
+         * Request mode
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#mode
+         */
+        mode: 'cors' | 'no-cors' | 'same-origin';
+
+        /**
+         * Priority of the fetch request relative to other requests
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#priority
+         */
+        priority: 'high' | 'low' | 'auto';
+
+        /**
+         * How to handle redirect response
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#redirect
+         */
+        redirect: 'follow' | 'error' | 'manual';
+
+        /**
+         * Referrer of the request
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#referrer
+         */
+        referrer: string;
+
+        /**
+         * Referrer policy to use for the request
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#referrerpolicy
+         */
+        referrerPolicy: 'no-referrer' | 'no-referrer-when-downgrade' | 'same-origin' | 'origin' | 'strict-origin'
+            | 'origin-when-cross-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url';
+
+        /**
+         * AbortSignal object instance. Allows to communicate with a fetch
+         * request and abort it if desired via an AbortController
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/fetch#referrerpolicy
+         */
+        signal: AbortSignal;
+    }>
 }> & (P extends null ? {} : {
     /** Path parameters */
     path: P;
