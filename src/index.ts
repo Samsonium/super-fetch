6c63537de41157ffe7f9d0930a86c1614d7a6e2c
samsonium <tdesu@vk.com>
2024-02-29 00:40:05 +0300

    Update index file

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+// Errors
+export {ApiUrlError} from './errors/ApiUrlError';
+export {ApiParamsError} from './errors/ApiParamsError';
+export {ApiResponseError} from './errors/ApiResponseError';
+
+// Fetchers
+export {fetchApi} from './fetchers/fetchAPI';
+
+// API repository
+export {ApiRecord} from './repo/ApiRecord';
+export {ApiRequestInit} from './repo/ApiRequestInit';
+export {FetchResponse} from './repo/FetchResponse';
