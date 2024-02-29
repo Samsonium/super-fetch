ef1d33ed646aadb64fc1004380ba5346084e8777
Samsonium <tdesu@vk.com>
2024-02-29 09:52:36 +0300

    Remove `FMock` type because of useless

--- test/global.d.ts
+++ test/global.d.ts
@@ -1,10 +1,6 @@
 import { Mock } from 'vitest';
 
 declare global {
-
-    /** Fetch function */
-    type FMock = Mock<['input', 'init'], Promise<Response>>;
-
     /** Create mocked response */
     function createResponse(code: number, text: string, data: Record<string, any>): Response;
 }
