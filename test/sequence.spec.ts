9dcdce388b948a3517beca19353b722add44941f
samsonium <tdesu@vk.com>
2024-03-11 01:54:53 +0300

    Add tests for Sequence fetcher

---
+++ test/sequence.spec.ts
@@ -0,0 +1,54 @@
+/// <reference path="global.d.ts" />
+import { describe, test, expect, beforeEach, Mock } from 'vitest';
+import sf, { ApiRecord } from '../src';
+
+// Declare mocked fetch
+declare const fetch: Mock<[URL, RequestInit?], Promise<Response>>;
+
+const apiRepo = {
+    simpleGET: {
+        method: 'GET',
+        endpoint: 'https://example.com/'
+    } as ApiRecord<{
+        page: number;
+        per_page: number;
+    }, null, {
+        items: string[]
+    }>
+};
+
+beforeEach(() => {
+    fetch.mockReset();
+});
+
+describe('Sequence', () => {
+    test('Request works', async () => {
+        const seq = new sf.Sequence(apiRepo.simpleGET);
+
+        fetch.mockResolvedValue(createResponse(200, 'OK', {items: ['hello']}));
+        await seq.call();
+        expect(seq.read()?.data).toStrictEqual({items: ['hello']});
+
+        fetch.mockResolvedValue(createResponse(200, 'OK', {items: ['!']}));
+        await seq.call();
+        expect(seq.read()?.data).toStrictEqual({items: ['!']});
+    });
+
+    test('Sequence.read returns last data', async () => {
+        const seq = new sf.Sequence(apiRepo.simpleGET);
+
+        fetch.mockResolvedValue(createResponse(200, 'OK', {items: ['hello']}));
+        await seq.call();
+        expect(seq.read()?.data).toStrictEqual({items: ['hello']});
+
+        fetch.mockResolvedValue(createResponse(200, 'OK', {items: ['world']}));
+        seq.call().then();
+        expect(seq.read()?.data).toStrictEqual({items: ['hello']});
+
+        fetch.mockResolvedValue(createResponse(200, 'OK', {items: ['!']}));
+        seq.call().then();
+        await new Promise(r => setTimeout(r, 1000));
+
+        expect(seq.read()?.data).toStrictEqual({items: ['!']});
+    });
+});
