715152d8c4ea35bc044f6b637f823fda8a06eed7
Samsonium <tdesu@vk.com>
2024-03-15 14:11:16 +0300

    Implement new fields in LongPolling fetcher

--- src/fetchers/LongPolling.ts
+++ src/fetchers/LongPolling.ts
@@ -9,22 +9,50 @@ export class LongPolling<Q = any, P = any, SR = any, ER = any, B = any> {
     /** Long-polling options */
     private readonly options: LongPollingOptions<Q, P, SR, ER, B>;
 
+    /** Poll step handler */
+    private readonly pollHandler: PollStep<SR, ER>;
+
+    /** Long-polling state */
+    private pollingStarted: boolean;
+
+    /** Number of consecutive timeouts */
+    private timeouts: number;
+
     /**
      * Create long-polling instance
      * @param url Endpoint URL or ApiRecord
+     * @param pollHandler Poll response handler. A Boolean in the
+     *                    return type indicates the need to continue
+     *                    polling. If `true` - continue poll, else stop
      * @param options Long-polling options
      */
     public constructor(
         url: ApiRecord<Q, P, SR, ER, B> | URL | string,
+        pollHandler: PollStep<SR, ER>,
         options?: Partial<LongPollingOptions<Q, P, SR, ER, B>>
     ) {
         options ??= {};
+
+        // If url is not ApiRecord
+        if (typeof url === 'string' || url instanceof URL) {
+            url = {
+                method: 'GET',
+                endpoint: url instanceof URL ? url.toString() : url
+            };
+        }
+
+        // Set variables
         this.options = {
             url,
             autoStart: true,
             delay: 1000,
             timeout: 20,
+            timeoutRetries: 3,
+            request: {} as ApiRequestInit<Q, P, B>,
             ...options
         };
+        this.pollHandler = pollHandler;
+        this.pollingStarted = false;
+        this.timeouts = 0;
     }
 }
